62427f26ddcaf856d74f2446d7e7024aed5cdbaa
Adam Fisher <fisher1987@gmail.com>
2016-02-03 13:23:21 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# RestSharp.Newtonsoft.Json
-Restores Newtonsoft.JSON as the default serializer for RestSharp.
+# RestSharp.Newtonsoft.JSON
+
+This is a glue library that makes [Newtonsoft.JSON](https://github.com/JamesNK/Newtonsoft.Json) the default serializer for [RestSharp](https://github.com/restsharp/RestSharp). 
+
+RestSharp removed Newtonsoft.JSON as a dependency in [version 103.0](https://github.com/restsharp/RestSharp/blob/master/releasenotes.markdown#1030---remove-dependency-on-jsonnet) which is great if you don't need all the extra capability Newtonsoft.JSON provides, most notably property renaming/mapping properties using the `[JsonProperty]` attribute.
+
+After working on several projects and getting annoyed with the limited functionality available in the default JsonSerializer include with .NET, I decided to create this project so I wouldn't have to leave a JSON serialization implementation laying around in my projects. Hopefully it helps someone else too.
+
+## Getting Started
+
+### Installation
+
+This package is available as a [**nuget package**](https://www.nuget.org/packages/RestSharp.Newtonsoft.Json) on nuget.org.
+
+### Code
+
+This library makes a `NewtonsoftJsonSerializer` available which you can readily plug into your RestSharp request object like this:
+
+```csharp
+var request = new RestRequest();
+request.JsonSerializer = new NewtonsoftJsonSerializer();
+```
+
+If you don't want to keep initializing every `RestRequest`, you can also use the **`RestSharp.Newtonsoft.Json.RestRequest`** class instead of the one from RestSharp. They are named the same but this class will default to using the Newtonsoft.JSON serialization engine.
---
+++ RestSharp.Newtonsoft.Json/RestRequest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace RestSharp.Newtonsoft.Json
+{
+    /// <summary>
+    /// Container for data used to make requests.
+    /// </summary>
+    public class RestRequest : RestSharp.RestRequest
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestRequest"/> class.
+        /// </summary>
+        public RestRequest()
+        {
+            IntializeJsonSerializer();
+        }
+
+        /// <summary>
+        /// Sets Method property to value of method
+        /// 
+        /// </summary>
+        /// <param name="method">Method to use for this request</param>
+        public RestRequest(Method method) : base(method)
+        {
+            IntializeJsonSerializer();
+        }
+
+        /// <summary>
+        /// Sets Resource property
+        /// 
+        /// </summary>
+        /// <param name="resource">Resource to use for this request</param>
+        public RestRequest(string resource) : base(resource)
+        {
+            IntializeJsonSerializer();
+        }
+
+        /// <summary>
+        /// Sets Resource and Method properties
+        /// 
+        /// </summary>
+        /// <param name="resource">Resource to use for this request</param><param name="method">Method to use for this request</param>
+        public RestRequest(string resource, Method method) : base(resource, method)
+        {
+            IntializeJsonSerializer();
+        }
+
+        /// <summary>
+        /// Sets Resource property
+        /// 
+        /// </summary>
+        /// <param name="resource">Resource to use for this request</param>
+        public RestRequest(Uri resource) : base(resource)
+        {
+            IntializeJsonSerializer();
+        }
+
+        /// <summary>
+        /// Sets Resource and Method properties
+        /// 
+        /// </summary>
+        /// <param name="resource">Resource to use for this request</param><param name="method">Method to use for this request</param>
+        public RestRequest(Uri resource, Method method) : base(resource, method)
+        {
+            IntializeJsonSerializer();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Intializes the serializer.
+        /// </summary>
+        protected void IntializeJsonSerializer()
+        {
+            JsonSerializer = new NewtonsoftJsonSerializer();
+        }
+
+        #endregion
+    }
+}
